1d18289bd53461abe177278f5b861654c8750979
Ninkua <ninkuaff@gmail.com>
2021-08-02 19:11:23 -0300

    completed

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,9 +6,15 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.headers;
-    const all = this.listAllUsersUseCase.execute({ user_id: String(user_id) });
-    return response.json(all);
+    try {
+      const { user_id } = request.headers;
+      const all = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
+      return response.json(all);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,9 +6,13 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.params;
-    const user = this.turnUserAdminUseCase.execute({ user_id });
-    return response.status(200).json(user);
+    try {
+      const { user_id } = request.params;
+      const user = this.turnUserAdminUseCase.execute({ user_id });
+      return response.status(200).json(user);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
